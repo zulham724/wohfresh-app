104ea79b8f995dac03e8b6d6d997707b6b1684fb
anonymous <creatorb45@gmail.com>
2018-08-28 16:14:18 +0700

    bismillah 1st commit

---
+++ app/src/main/java/com/woohfresh/Splashbe.java
@@ -0,0 +1,13 @@
+package com.woohfresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashbe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashbe);
+    }
+}
